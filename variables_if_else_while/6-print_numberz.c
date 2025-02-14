2349664cae5258a9671758d8bbdf375baaa31954
JulienGirardey <juliengirardey2@gmail.com>
2025-02-14 13:40:14 +0000

    Task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Fonction main
+ *
+ * Return: 0(sucess)
+ */
+int main(void)
+{
+	int i;
+
+	i = 48;
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
