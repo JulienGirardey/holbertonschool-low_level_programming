bdf89153a4186410a9b13c1397fb9c66f7c7418b
JulienGirardey <juliengirardey2@gmail.com>
2025-02-14 13:04:01 +0000

    Task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Fonction main
+ *
+ * Retrun: 0(sucess)
+ */
+int main()
+{
+	int i;
+
+	i = 97;
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
