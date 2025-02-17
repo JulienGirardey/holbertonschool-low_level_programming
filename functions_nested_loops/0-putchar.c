27efceb54b97a6ee24f0a84bcb08a86ce0082684
JulienGirardey <juliengirardey2@gmail.com>
2025-02-17 08:17:23 +0000

    Task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Fonction main
+ *
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Fonction main
+ *
+ * Return: 0 (Sucess)
+ */
+int _putchar(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
