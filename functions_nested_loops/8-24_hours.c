1f6742eb5bce620d7f2d749ea525c9a90e53585d
JulienGirardey <juliengirardey2@gmail.com>
2025-02-19 09:15:49 +0000

    Task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * jack_bauer - Display horloge count
@@ -9,50 +8,26 @@
 void jack_bauer(void)
 {
 	int min = 0;
-	int min2 = 0;
+	int min2 = -1;
 	int houre = 0;
 	int houre2 = 0;
 
-	_putchar('0' + houre);
-	_putchar('0' + houre2);
-	_putchar(':');
-	_putchar('0' + min);
-	_putchar('0' + min2);
-	_putchar('\n');
 	while (houre != 2 || houre2 != 3 || min != 5 || min2 != 9)
 	{
 		if (min2 < 9)
 		{
 			min2++;
-			_putchar('0' + houre);
-			_putchar('0' + houre2);
-			_putchar(':');
-			_putchar('0' + min);
-			_putchar('0' + min2);
-			_putchar('\n');
 		}
 		else if (min < 5)
 		{
 			min2 = 0;
 			min++;
-			_putchar('0' + houre);
-			_putchar('0' + houre2);
-			_putchar(':');
-			_putchar('0' + min);
-			_putchar('0' + min2);
-			_putchar('\n');
 		}
 		else if (houre2 < 9)
 		{
 			min = 0;
 			min2 = 0;
 			houre2++;
-			_putchar('0' + houre);
-			_putchar('0' + houre2);
-			_putchar(':');
-			_putchar('0' + min);
-			_putchar('0' + min2);
-			_putchar('\n');
 		}
 		else
 		{
@@ -60,12 +35,12 @@ void jack_bauer(void)
 			min = 0;
 			min2 = 0;
 			houre++;
-			_putchar('0' + houre);
-			_putchar('0' + houre2);
-			_putchar(':');
-			_putchar('0' + min);
-			_putchar('0' + min2);
-			_putchar('\n');
 		}
+		_putchar('0' + houre);
+		_putchar('0' + houre2);
+		_putchar(':');
+		_putchar('0' + min);
+		_putchar('0' + min2);
+		_putchar('\n');
 	}
 }
