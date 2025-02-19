4a8502eb82bd87bd9f3e430a2c4ba48aa2bf1b3f
JulienGirardey <juliengirardey2@gmail.com>
2025-02-19 15:33:02 +0000

    Task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,29 +12,21 @@ void times_table(void)
 	{
 		while (product < 10)
 		{
-			if (product == 9)
-			{
-				if (factor * product >= 10)
-				{
-					_putchar('0' + factor * product / 10);
-					_putchar('0' + factor * product % 10);
-				}
-				else
-					_putchar('0' + factor * product);
-			}
+			if (product == 0)
+				_putchar('0' + factor * product);
 			else if (factor * product >= 10)
 			{
-				_putchar('0' + factor * product / 10);
-				_putchar('0' + factor * product % 10);
 				_putchar(',');
 				_putchar(' ');
+				_putchar('0' + factor * product / 10);
+				_putchar('0' + factor * product % 10);
 			}
 			else if (factor * product <= 10)
 			{
-				_putchar('0' + factor * product);
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
+				_putchar('0' + factor * product);
 			}
 			product++;
 		}
