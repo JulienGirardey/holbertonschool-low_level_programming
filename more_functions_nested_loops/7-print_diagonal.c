b7076fbf64d9d7e8f3dd4db329c4c8f2365de610
JulienGirardey <juliengirardey2@gmail.com>
2025-02-25 08:57:13 +0000

    Task 7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,4 +9,29 @@
  */
 void print_diagonal(int n)
 {
+	int nbSpace;
+	int i = 0;
+	int j = 0;
+
+	while (i < n)
+	{
+		if (i > 0)
+		{
+			j = i;
+			nbSpace = 0;
+			while (j > nbSpace)
+			{
+				_putchar(' ');
+				nbSpace++;
+			}
+		}
+		if (n > 0)
+		{
+			_putchar('\\');
+			_putchar('\n');
+		}
+		i++;
+	}
+	if (n == 0 || n < 0)
+		_putchar('\n');
 }
