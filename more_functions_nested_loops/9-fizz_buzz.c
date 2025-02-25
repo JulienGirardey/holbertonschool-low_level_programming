8229a35f7121f9a3e195262a6dfdceab3c233b2c
JulienGirardey <juliengirardey2@gmail.com>
2025-02-25 12:17:52 +0000

    Task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,10 +13,12 @@ void fizzBuzz(void)
 
 	while (i <= 100)
 	{
-		if (i % 3 == 0)
-			printf("Fizz ");
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
 		else if (i % 5 == 0)
 			printf("Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
 		else
 			printf("%d ", i);
 		i++;
