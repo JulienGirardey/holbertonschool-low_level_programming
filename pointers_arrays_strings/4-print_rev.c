e339db9e86ea1d577f8b8b1cff0ead3d4ab8a842
JulienGirardey <juliengirardey2@gmail.com>
2025-02-26 09:26:36 +0000

    Task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - Reverse a string
+ *
+ * @s: String to reverse
+ *
+ * Return: Void.
+ */
+void print_rev(char *s)
+{
+	int end = 0;
+
+	while (s[end])
+		end++;
+	end--;
+	while (end > 0)
+	{
+		_putchar(s[end]);
+		end--;
+	}
+	_putchar('\n');
+}
