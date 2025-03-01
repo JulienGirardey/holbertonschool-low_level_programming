ed0723179576f7aec3bc98b572c4cae150d106a2
JulienGirardey <juliengirardey2@gmail.com>
2025-03-01 14:24:29 +0100

    Task 4.2

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* reverse_array - Reverse the content of an array
+*
+* @a: Array to reverse
+* @n: Number of elements
+*
+* Return: Void.
+*/
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+
+	int box = 0;
+
+	while (a[i] < a[n - 1])
+	{
+		box = a[i];
+		a[i] = a[n - 1];
+		a[n - 1] = box;
+		i++;
+		n--;
+	}
+}
