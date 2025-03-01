a2ac03213ef62f9a6f61423ace48bd142fbc372c
JulienGirardey <juliengirardey2@gmail.com>
2025-03-01 11:31:54 +0100

    Task 2.2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -14,13 +14,8 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	if (n == 0)
-		return (dest);
-	while (i < n)
-	{
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-		i++;
-	}
 	while (i < n)
 	{
 		dest[i] = '\0';
