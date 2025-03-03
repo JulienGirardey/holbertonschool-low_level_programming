7640b232346d3e7f2f764ca752e7359fca751c3c
JulienGirardey <juliengirardey2@gmail.com>
2025-03-03 14:40:28 +0100

    Task 2.3

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -12,11 +12,13 @@
 */
 char *_strchr(char *s, char c)
 {
-	if (*s == '\0')
-		return (s);
-	while (*s != c)
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		if (*s == '\0')
+			return (NULL);
 		s++;
-	if (*s == '\0')
-		return (NULL);
-	return (s);
+	}
+	return (NULL);
 }
