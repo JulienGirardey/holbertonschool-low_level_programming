a4f6c7794d48a7572a76749bd98960c62a573caf
JulienGirardey <juliengirardey2@gmail.com>
2025-03-12 09:29:42 +0100

    Task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - information about dog
+ *
+ * @name: Name of dog.
+ * @age: Age of dog.
+ * @owner: Owner of dog.
+ *
+ * Description: This struct define 3 variables who containes 1 informations
+ * about dog : this name, age and owner.
+ */
+struct dog
+{
+	char *name;
+
+	float age;
+
+	char *owner;
+
+};
+
+#endif
