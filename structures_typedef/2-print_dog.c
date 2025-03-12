c3e273bcc38efc61c50260117898e3d8dff24114
JulienGirardey <juliengirardey2@gmail.com>
2025-03-12 10:14:01 +0100

    Task 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,7 +14,11 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		printf("\n");
 	else if (d->name == NULL)
-	    printf("Name: (nil)\nAge: %f\nOwner: %s\n", d->age, d->owner);
+	    printf("Name: nil\nAge: %f\nOwner: %s\n", d->age, d->owner);
+	else if (d->age == -1.0)
+		printf("Name: %s\nAge: nil\nOwner: %s\n", d->name, d->owner);
+	else if (d->owner == NULL)
+		printf("Name: %s\nAge: %f\nOwner: nil\n", d->name, d->age);
 	else
 		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
