dad73f1e9ce67012ac66d2cb15b97b488da8de97
JulienGirardey <juliengirardey2@gmail.com>
2025-03-12 13:35:54 +0100

    Task 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+* free_dog - That frees the allocation for dogs struct.
+*
+* @d: struct of dogs.
+*
+* Return: Always void.
+*/
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+	exit(EXIT_SUCCESS);
+}
