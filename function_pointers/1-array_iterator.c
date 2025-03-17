df35ada4298365fa8ea59d5fd6aa7a09a097661f
JulienGirardey <juliengirardey2@gmail.com>
2025-03-17 14:33:02 +0100

    Task 1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -13,12 +13,11 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
+	size_t i;
 
-	if (array == NULL)
-		return;
-	if (action == NULL)
-		return;
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (action != NULL && array != NULL)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
 }
