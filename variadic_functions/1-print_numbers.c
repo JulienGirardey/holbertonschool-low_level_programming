ff91cf84824ea8756bc21b14566dcd4cec47193b
JulienGirardey <juliengirardey2@gmail.com>
2025-03-19 10:25:19 +0100

    Task 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
 * print_numbers - That prints numbers, followed by a new line.
@@ -17,6 +18,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_list args;
 
+	if (n == 0)
+	{
+		printf("\n");
+		va_end(args);
+		exit(EXIT_FAILURE);
+	}
 	va_start(args, n);
 	while (i <= n)
 	{
