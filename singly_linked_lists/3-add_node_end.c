bbefb4fba89a2989a38b38d3e64116ccea8cca64
JulienGirardey <juliengirardey2@gmail.com>
2025-03-31 16:17:18 +0200

    add task 3 and list.h file with news prototypes

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+* add_node_end - adds a new node at the end of a list
+*
+* @head: new list
+* @str: string to add in the list
+*
+* Return: new list
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *last = *head;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (NULL);
+	}
+
+	while (last->next != NULL)
+		last = last->next;
+
+	last->next = new;
+	return (last);
+}
