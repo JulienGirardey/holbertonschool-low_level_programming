78aa5b7e841e243ccd0f97feed6e29131f5dffbe
JulienGirardey <juliengirardey2@gmail.com>
2025-03-31 11:44:15 +0200

    add 1 task file to count the numbers of elements in the list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* list_len - Count the numbers of elements in a linked list_t list
+*
+* @h: list to count
+*
+* Return: Numbers of elements count
+*/
+size_t list_len(const list_t *h)
+{
+	unsigned int i = 1;
+
+	while (h->next != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
