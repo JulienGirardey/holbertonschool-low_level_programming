b8d7a64fdd31e13e036a8a57e893ed53cc72de00
JulienGirardey <juliengirardey2@gmail.com>
2025-04-01 09:58:01 +0200

    fix add_node function

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -17,9 +17,17 @@ list_t *add_node(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
+
 	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
 	new->len = strlen(str);
 	new->next = (*head);
 	(*head) = new;
-	return (new);
+
+	return (new->next);
 }
