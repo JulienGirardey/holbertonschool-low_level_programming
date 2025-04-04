977b092c96a095aa3fab2a2f91482919243d50d0
JulienGirardey <juliengirardey2@gmail.com>
2025-04-04 14:32:08 +0200

    add new condition, if text_content == null create an empty file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,6 +19,9 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content == NULL)
+		text_content = "";
+
 	fd = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
