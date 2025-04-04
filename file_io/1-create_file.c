0f1041be4c13e0976ba70755c8c88e648e56bde6
JulienGirardey <juliengirardey2@gmail.com>
2025-04-04 14:42:05 +0200

    add O_TRUNC

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	fd = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
 		close(fd);
