a9df6ed2cd0d0147807368f8ec344c25aa17226f
JulienGirardey <juliengirardey2@gmail.com>
2025-04-04 14:23:34 +0200

    add function to create a new file with specific permission and something in

---
+++ file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <fcntl.h>
+#include <string.h>
+
+/**
+* create_file - that creates a file
+*
+* @filename: name of the new file
+* @text_content: content of the new file
+*
+* Return: 1 on success, -1 on failure
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	ssize_t bytesWritten;
+
+	fd = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	bytesWritten = write(fd, text_content, strlen(text_content));
+
+	if (bytesWritten == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
